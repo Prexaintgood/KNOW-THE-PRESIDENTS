9eda71105233c3b57a27ff10dfa448714f94ec4f
PREX <144615164+Prexaintgood@users.noreply.github.com>
2024-06-26 23:39:51 +0800

    Delete KNOWS THE PRESIDENT directory

--- KNOWS THE PRESIDENT/script.js
+++
@@ -1,8 +0,0 @@
-function handleSuggestionSubmit() {
-    var suggestion = document.getElementById("suggestion").value;
-    if (suggestion.trim() === "") {
-        alert("Please fill out the suggestion box.");
-    } else {
-        alert("Thank you for your suggestion!");
-    }
-}
